e3235c01e5295f5898c7a8e39ac783a9d08b4304
rjeeb <rafat.albarouki@gmail.com>
2019-07-30 11:20:21 +0300

    add global interceptors and default fail handler

--- domino-rest/src/main/java/org/dominokit/domino/rest/DominoRestConfig.java
+++ domino-rest/src/main/java/org/dominokit/domino/rest/DominoRestConfig.java
@@ -3,7 +3,19 @@ package org.dominokit.domino.rest;
 import org.dominokit.domino.rest.android.AndroidRequestSender;
 import org.dominokit.domino.rest.android.DefaultServiceRoot;
 import org.dominokit.domino.rest.android.OnServerRequestEventFactory;
-import org.dominokit.domino.rest.shared.request.*;
+import org.dominokit.domino.rest.shared.request.AsyncRunner;
+import org.dominokit.domino.rest.shared.request.DefaultRequestAsyncSender;
+import org.dominokit.domino.rest.shared.request.DominoRestContext;
+import org.dominokit.domino.rest.shared.request.DynamicServiceRoot;
+import org.dominokit.domino.rest.shared.request.Fail;
+import org.dominokit.domino.rest.shared.request.GlobalResponseHandler;
+import org.dominokit.domino.rest.shared.request.RequestInterceptor;
+import org.dominokit.domino.rest.shared.request.RequestRouter;
+import org.dominokit.domino.rest.shared.request.RestConfig;
+import org.dominokit.domino.rest.shared.request.ServerRequest;
+import org.dominokit.domino.rest.shared.request.ServerRouter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +25,7 @@ import static java.util.Objects.nonNull;
 
 public class DominoRestConfig implements RestConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DominoRestConfig.class);
     private static String defaultServiceRoot;
     private static String defaultResourceRootPath = "service";
     private static String defaultJsonDateFormat = null;
@@ -21,6 +34,14 @@ public class DominoRestConfig implements RestConfig {
             new OnServerRequestEventFactory(), new AndroidRequestSender<>()));
     private static List<DynamicServiceRoot> dynamicServiceRoots = new ArrayList<>();
     private static List<RequestInterceptor> interceptors = new ArrayList<>();
+    private static final List<GlobalResponseHandler> globalResponseHandlers = new ArrayList<>();
+    private static Fail defaultFailHandler = failedResponse -> {
+        if (nonNull(failedResponse.getThrowable())) {
+            LOGGER.debug("could not execute request on server: ", failedResponse.getThrowable());
+        } else {
+            LOGGER.debug("could not execute request on server: status [" + failedResponse.getStatusCode() + "], body [" + failedResponse.getBody() + "]");
+        }
+    };
 
     public static DominoRestConfig initDefaults() {
         RestfullRequestContext.setFactory(new AndroidRestfulRequestFactory());
@@ -61,6 +82,21 @@ public class DominoRestConfig implements RestConfig {
         return interceptors;
     }
 
+    public DominoRestConfig addGlobalResponseHandler(GlobalResponseHandler globalResponseHandler) {
+        this.getGlobalResponseHandlers().add(globalResponseHandler);
+        return this;
+    }
+
+    public DominoRestConfig removeGlobalResponseHandler(GlobalResponseHandler globalResponseHandler) {
+        this.getGlobalResponseHandlers().remove(globalResponseHandler);
+        return this;
+    }
+
+    @Override
+    public List<GlobalResponseHandler> getGlobalResponseHandlers() {
+        return globalResponseHandlers;
+    }
+
     public String getDefaultServiceRoot() {
         if (isNull(defaultServiceRoot)) {
             return DefaultServiceRoot.get() + defaultResourceRootPath + "/";
@@ -93,6 +129,18 @@ public class DominoRestConfig implements RestConfig {
         } else {
             return "";
         }
+    }
+
+    public DominoRestConfig setDefaultFailHandler(Fail fail) {
+        if (nonNull(fail)) {
+            this.defaultFailHandler = fail;
+        }
+        return this;
+    }
+
+    @Override
+    public Fail getDefaultFailHandler() {
+        return defaultFailHandler;
     }
 
     @Override
